0ca4155f05eff1fc080338c348dc402b67ebe7a1
Jorge Carrera <jocarrd@unirioja.es>
2021-11-21 12:19:52 +0100

    migrando-a-express

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const http= require('http')
-
+const { request, response } = require('express')
+const express= require('express')
+const app= express()
 
 let json= [{"id" : 1, "content" : "esto es una prueba"}]
-
+/*
 const app= http.createServer((request,response)=>{
     response.writeHead(200,{'Content-Type': 'aplication/json'})
     response.end(JSON.stringify(json))
+})
+*/
+
+app.get('/',(request,response)=>{
+    response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/content',(request,response)=>{
+    response.json(json)
 })
 
 const PORT=3002
