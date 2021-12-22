653454894d1fee4ee6550a31bad661a86596b69a
Jorge Carrera Diez <55456507+jocarrd@users.noreply.github.com>
2021-12-22 12:27:37 +0100

    loger

---
+++ logger-Middleware.js
@@ -0,0 +1,11 @@
+ const logger =((request,response,next)=>{
+    console.log(request.method)
+    console.log(request.path)
+    console.log(request.body)
+    console.log("------------")
+    
+    next() //Continua 
+
+})
+
+module.exports=logger 
