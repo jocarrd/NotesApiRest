8948e0760b9a7e732880753526fe52f464b6b5cf
Jorge Carrera Diez <55456507+jocarrd@users.noreply.github.com>
2021-12-22 11:54:46 +0100

    eslint-install

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}
--- index.js
+++ index.js
@@ -1,66 +1,63 @@
-const { request, response } = require('express')
-const express= require('express')
-const app= express()
-app.use(express.json())
-
-let json= [{"id" : 1, "content" : "esto es una prueba"}]
-
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+app.use(express.json());
+let json = [{ id: 1, content: "esto es una prueba" }];
 /*
 const app= http.createServer((request,response)=>{
     response.writeHead(200,{'Content-Type': 'aplication/json'})
     response.end(JSON.stringify(json))
 })
 */
-
-app.get('/',(request,response)=>{
-    response.send('<h1>Hello world</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
 
 // GET ALL
-app.get('/api/content',(request,response)=>{
-    response.json(json)
-})
+app.get("/api/content", (request, response) => {
+  response.json(json);
+});
 
 //GET ONE
-app.get('/api/content/:id',(request,response)=>{
-    const id = Number( request.params.id)
-    const content = json.find(content => content.id === id )
-    if(content){
-    response.json(content)
-    }else{
-        response.status(404).end()
-    }
-})
+app.get("/api/content/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const content = json.find((content) => content.id === id);
+  if (content) {
+    response.json(content);
+  } else {
+    response.status(404).end();
+  }
+});
 
 //DELETE ONE
-app.delete('/api/content/:id',(request,response)=>{
-    const id = Number( request.params.id)
-    json = json.filter(note => note.id =! id)
-    response.status(204).end 
-})
+app.delete("/api/content/:id", (request, response) => {
+  const id = Number(request.params.id);
+  json = json.filter((note) => (note.id = !id));
+  response.status(204).end;
+});
 
 //POST
-app.post('/api/content', (request,response)=>{
-    const note = request.body //Obtención de los parámetros de la petición
-    //Busqueda del max id y creación de un id
-    if(!note || !note.content){
+app.post("/api/content", (request, response) => {
+  const note = request.body; //Obtención de los parámetros de la petición
+  //Busqueda del max id y creación de un id
+  if (!note || !note.content) {
+    return response.status(400).json({
+      error: "note content is missing",
+    });
+  }
 
-        return response.status(400).json({
-            error: 'note content is missing'
-        })
-    }
+  const ids = json.map((note) => note.id);
+  const maxId = Math.max(...ids);
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+  };
+  //Añade la nueva nota a las notas
+  json = [...json, newNote];
+  response.json(newNote);
+});
 
-
-    const ids= json.map(note=> note.id)
-    const   maxId= Math.max(...ids)    
-    const newNote={
-        id: maxId+1 ,
-        content: note.content
-    }
-    //Añade la nueva nota a las notas
-    json=[...json,newNote]
-    response.json(newNote)
-})
-
-const PORT=3002
-app.listen(PORT , ()=>{console.log(`Server running on port ${PORT}`)})
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
